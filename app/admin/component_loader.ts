f01cf4b5252351af58548e1cfb5d3d8deb3f53fe
princelulinda <princelulinda@gmail.com>
2026-05-20 04:11:25 +0200

    new

--- app/admin/component_loader.ts
+++ app/admin/component_loader.ts
@@ -1,16 +1,15 @@
 import { ComponentLoader } from 'adminjs'
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const componentLoader = new ComponentLoader()
 
+const rootPath = process.cwd()
+
 export const COMPONENTS = {
-  Dashboard: componentLoader.add('Dashboard', path.join(__dirname, './components/Dashboard.jsx')),
-  GenerateSignalComponent: componentLoader.add('GenerateSignalComponent', path.join(__dirname, './components/GenerateSignalComponent.jsx')),
-  ImageUploadComponent: componentLoader.add('ImageUploadComponent', path.join(__dirname, './components/ImageUploadComponent.jsx')),
-  ConsolidationReport: componentLoader.add('ConsolidationReport', path.join(__dirname, './components/ConsolidationReport.jsx')),
+  Dashboard: componentLoader.add('Dashboard', path.join(rootPath, 'app/admin/components/Dashboard.jsx')),
+  GenerateSignalComponent: componentLoader.add('GenerateSignalComponent', path.join(rootPath, 'app/admin/components/GenerateSignalComponent.jsx')),
+  ImageUploadComponent: componentLoader.add('ImageUploadComponent', path.join(rootPath, 'app/admin/components/ImageUploadComponent.jsx')),
+  ConsolidationReport: componentLoader.add('ConsolidationReport', path.join(rootPath, 'app/admin/components/ConsolidationReport.jsx')),
 }
 
 export default componentLoader
