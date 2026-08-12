ed72c5e994158db1435ea9e675828f96f961ca81
princelulinda <princelulinda@gmail.com>
2026-08-12 14:05:48 +0200

    resolve component admin

--- app/admin/component_loader.ts
+++ app/admin/component_loader.ts
@@ -6,10 +6,10 @@ const componentLoader = new ComponentLoader()
 const rootPath = process.cwd()
 
 export const COMPONENTS = {
-  Dashboard: componentLoader.add('Dashboard', path.join(rootPath, 'app/admin/components/Dashboard.jsx')),
-  GenerateSignalComponent: componentLoader.add('GenerateSignalComponent', path.join(rootPath, 'app/admin/components/GenerateSignalComponent.jsx')),
-  ImageUploadComponent: componentLoader.add('ImageUploadComponent', path.join(rootPath, 'app/admin/components/ImageUploadComponent.jsx')),
-  ConsolidationReport: componentLoader.add('ConsolidationReport', path.join(rootPath, 'app/admin/components/ConsolidationReport.jsx')),
+  // Dashboard: componentLoader.add('Dashboard', path.join(rootPath, 'app/admin/components/Dashboard.jsx')),
+  // GenerateSignalComponent: componentLoader.add('GenerateSignalComponent', path.join(rootPath, 'app/admin/components/GenerateSignalComponent.jsx')),
+  // ImageUploadComponent: componentLoader.add('ImageUploadComponent', path.join(rootPath, 'app/admin/components/ImageUploadComponent.jsx')),
+  // ConsolidationReport: componentLoader.add('ConsolidationReport', path.join(rootPath, 'app/admin/components/ConsolidationReport.jsx')),
 }
 
 export default componentLoader
